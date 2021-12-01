ad3fc32631b7347c91714a61cbd61404c41ef810
Amy <amyrogers390@gmail.com>
2021-12-02 00:08:02 +1300

    modified day 1

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -17,17 +17,12 @@ with open("input.txt", "r") as fp:
     submarine_depths = [int(line) for i, line in enumerate(fp)]
 fp.close()
 
+#since two numbers are the same in the rolling average, should be able to just compare if
+#lst[i] < lst[i+3]
 higher_numbers = 0
-prev_rolling_sum = 0
-for i in range(0,len(submarine_depths)):
-    if i >= 2 and i <= len(submarine_depths)-2:
-        rolling_sum = submarine_depths[i] + submarine_depths[i-1] + submarine_depths[i-2]
-        if rolling_sum > prev_rolling_sum:
-            print(f"{rolling_sum} is higher than {prev_rolling_sum}")
-            higher_numbers += 1
-        else:
-            print(f"{rolling_sum} is NOT higher than {prev_rolling_sum}")
-        prev_rolling_sum = rolling_sum
+for i in range(0,len(submarine_depths)-3):
+    if submarine_depths[i+3] > submarine_depths[i]:
+        higher_numbers += 1
 
+assert higher_numbers == 1739
 print(higher_numbers)
-        
